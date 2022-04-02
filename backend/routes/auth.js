9efcaa9cfa468455eeb22a1c6072ee7dcc517520
kwsoftware <kalpeshwankhede@outlook.com>
2022-04-02 17:13:02 +0530

    auth token done

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -2,24 +2,49 @@ const express = require('express');
 const User = require('../models/User');
 const router = express.Router();
 const { body, validationResult } = require('express-validator');
+const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
+const JWT_SECRET = "Iamtheboss";
 
 // Create a User using: POST "/api/auth/". Doesn't require Auth
-router.post('/', [
+router.post('/CreateUser', [
     body('name', 'Name should be at least 5 characters').isLength({ min: 5 }),
     body('empno', 'enter a valid Employee Number').isLength({min: 8}),
     body('password', 'Password should be at least 5 Characters').isLength({ min: 5 })
-], (req, res) => {
+], async(req, res) => {
+    //if there are errors, return bad request and error
     const errors = validationResult(req);
     if (!errors.isEmpty()) {
         return res.status(400).json({ errors: errors.array() });
     }
-    User.create({
-        name: req.body.name,
-        empno: req.body.empno,
-        password: req.body.password,
-    }).then(user => res.json(user))
-    .catch(err => {console.log(err)
-    res.json({error:'please enter unique value for empno',message:err.message})});
+    try{
+        //Check whether the user with same emp no already exist
+        let user = await User.findOne({empno:req.body.empno});
+        if (user) {
+            return res.status(400).json({error:"Sorry User with this empno already exist"});
+        }
+        const salt = await bcrypt.genSalt(10);
+        const secPass = await bcrypt.hash(req.body.password, salt);
+        // Create new User
+        user = await User.create({
+            name: req.body.name,
+            empno: req.body.empno,
+            password: secPass,
+        });
+        const data = {
+            user:{
+                id: user.id
+            }
+        }
+        const authtoken = jwt.sign(data, JWT_SECRET);
+        res.json({authtoken});
+
+    }catch(error){
+        console.error(error.message);
+        res.status(500).send('Some error occured');
+
+    }
+    
 
 });
 
